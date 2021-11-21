51181e4055d81527578d8d61e77a29d66f57b6fb
Akshat Shukla <akshatshukla175@gmail.com>
2021-11-21 16:03:58 +0530

    First Commit

--- studify/settings.py
+++ studify/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'studify.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
         # 'ENGINE': 'django.db.backends.mysql',
         # 'NAME' : 'student_management_system',
         # 'USER' : 'student_management_system',
